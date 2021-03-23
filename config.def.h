7a75756b9686f81c1bbdb670bec405d981ae321e
Nathaniel Evan <nathanielevan@zohomail.com>
2021-03-23 19:16:19 +0700

    Changes font size, schemesel bg colour and lineheight to match dwm status bar

--- config.def.h
+++ config.def.h
@@ -4,19 +4,19 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Nerd Font Mono:style=Bold:size=10.5"
+	"Iosevka Nerd Font:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#d8dee9", "#2e3440" },
-	[SchemeSel] = { "#2e3440", "#88c0d0" },
+	[SchemeSel] = { "#2e3440", "#81a1c1" },
 	[SchemeOut] = { "#2e3440", "#a3be8c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 29;
+static unsigned int lineheight = 25;
 static unsigned int min_lineheight = 8;
 
 /*
