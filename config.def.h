1f8363204387ef8bfdf379db6fa094a0ef6c7006
Nathaniel Evan <nathanielevan@zohomail.com>
2022-02-15 14:21:47 +0700

    Change font size, line height and selbg

--- config.def.h
+++ config.def.h
@@ -6,19 +6,19 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FantasqueSansMono Nerd Font:pixelsize=15:antialias=true:autohint=true"
+	"FantasqueSansMono Nerd Font:size=10:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#fff6e6", "#0b1726" },
-	[SchemeSel] = { "#0b1726", "#d9886a" },
+	[SchemeSel] = { "#0b1726", "#e9986d" },
 	[SchemeOut] = { "#0b1726", "#5d92bf" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 27;
+static unsigned int lineheight = 26;
 static unsigned int min_lineheight = 8;
 
 /*
